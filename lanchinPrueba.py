b981c46cb652e6cde4824b8fc83ed688682be38d
Itfriroth <78076229+Itfriroth@users.noreply.github.com>
2025-05-17 16:20:27 -0400

    Update lanchinPrueba.py

--- lanchinPrueba.py
+++ lanchinPrueba.py
@@ -1,9 +1,9 @@
-# esto es para usar en COLAB
+# This is for use in COLAB
 %%capture
 !pip install langchain pypdf openai chromadb tiktoken
 
 ------------
-#para ingresar los datos de CHATGPT
+#to enter the CHATGPT data
 from getpass import getpass
 import os
 
@@ -14,15 +14,14 @@ os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 import requests
 from langchain.document_loaders import PyPDFLoader
 
-#cuando se uso este ejemplo estos documentos estaban recien montados
-#por lo cual no le dio tiempo a revisarlos
-#tambien se pueden colocar archivos pdf
+#When this example was used, these documents were newly created, so there was no time to review them.
+#PDF files can also be uploaded.
 urls = [
-    'https://arxiv.org/pdf/2306.06031v1.pdf',
-    'https://arxiv.org/pdf/2306.12156v1.pdf',
-    'https://arxiv.org/pdf/2306.14289v1.pdf',
-    'https://arxiv.org/pdf/2305.10973v1.pdf',
-    'https://arxiv.org/pdf/2306.13643v1.pdf'
+    'url/1.pdf',
+    'url/2.pdf',
+    'url/3.pdf',
+    'url/4.pdf',
+    'url/5.pdf'
 ]
 
 ml_papers = []
@@ -37,22 +36,19 @@ for i, url in enumerate(urls):
         data=loader.load()
         ml_papers.extend(data)
 
-# Utiliza la lista ml_papers para acceder a los elementos de todos 
-#los documentos descargados
-print('Contenido de ml_papers:')
+# Use the ml_papers list to access the elements of all downloaded documents
+print('Contenido:')
 print()
 ----------------------------
-#aqui es para ver la longitud, tipo de documentos y seleccionar 
-#el de la posicion 4
+#here is to see the length, type of documents and select the one in position 4
 type(ml_papers), len(ml_papers), ml_papers[3]
 ------------
-------------
-#Cortar documentos(split) para poder hacer embebbing
+#Cut documents (split) to be able to embed
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1500,#tamaño del text
-    chunk_overlap=200,#caracteres de comparacion
+    chunk_size=1500,# text len
+    chunk_overlap=200,#comparison letters
     length_function=len
     )
 
@@ -60,7 +56,7 @@ documents = text_splitter.split_documents(ml_papers)
 ------------------------------------
 len(documents), documents[10]
 ------------------------------------
-#Embeddings e ingesta a base de datos vectorial
+#Embeddings and send to vectorial DB
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
@@ -73,11 +69,10 @@ vectorstore = Chroma.from_documents(
 )
 
 retriever = vectorstore.as_retriever(
-    search_kwargs={"k":3}#buscamos los 3 fragmentos que mas se parecen al query
-    #a mayor valor mayor gasto de token y talvez no quepa en el modelo
+    search_kwargs={"k":3}
     )
 --------------------------------------
-#Modelos de chat y cadenas para consulta de información
+#Chat templates and chains for information query
 
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
@@ -94,7 +89,7 @@ qa_chain = RetrievalQA.from_chain_type(
     retriever=retriever
 )
 -------------------------
-#estas son preguntas de datos encontrados en los documentos
+#These are questions about data found in the documents
 query = "qué es fingpt?"
 qa_chain.run(query)
 
@@ -106,21 +101,3 @@ qa_chain.run(query)
 
 query = "cuál es la diferencia entre fast sam y mobile sam?"
 qa_chain.run(query)
-
-Primero veria en que esta fallando, esta dando respuestas incoherentes? entonces esta alucinando y bajaria la temperatura a respuestas concretas. temperature=0.0
-es un chat que solo se basa en dar respuestas o un chatbot que guarda las interacciones anteriores?si es el primero reviso si usa embebbing y si el modelo es de texto
-Revisaria si esta usando base de datos vectorial
-
-First, I'd see what's wrong. Is he giving incoherent answers? Then he's hallucinating and I'd lower the temperature to concrete answers. Temperature = 0.0
-Is it a chat that only focuses on providing answers or a chatbot that saves previous interactions? If it's the former, I check if it uses embedding and if the model is text-based.
-I would check if there are using a vector database.But I need to know what error it gives and the use of the chat. Since a normal chat doesn't store previous responses, a chatbot does. It can also be created to search and analyze private information, among many other features.
-
-If there hallucinating the answers, I'd set the temperature to "0". temperature=0.0. I'd also add a chain_type="stuff" , which is to take everything from the prompt.
-
-
-comprobaria el output/max_tokens por la informacion incompleta y por el bloqueo comprobaria la interaccion, si el chat llama a todas las interacciones ya que al mayor cantidad de interacciones mas pesado, aqui es mejor hacer un resumen de las interacciones anteriores y solo colocar simpre las 5 ultimas.
-tambien 
-
-To reduce token usage I would limit input and output and to increase speed I would test different models and optimize memory.
-ultimate_chain=SequentialChain(chains=[chain1, chain2],input_variables=["text","stile"],output,variables=["final_text"])
-ultimate_chain({"text": input_text,"stile":"French"})
